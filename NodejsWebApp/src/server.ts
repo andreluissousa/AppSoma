6d4130ffae6aa5701401c0aa24376ec860d9aed7
andreluissousa <andreborges1777@gmail.com>
2024-04-22 15:37:27 -0300

    ix: chaging log to print value from varieble

--- NodejsWebApp/src/server.ts
+++ NodejsWebApp/src/server.ts
@@ -4,6 +4,6 @@ import app from "./app";
 const port = 3000;
 
 console.info("-------------------------------------------------------");
-console.info(chalk.blue(`Aplicação de soma rodando na porta ${3000}`));
+console.info(chalk.blue(`Aplicação de soma rodando na porta ${port}`));
 
 app.listen(port);
